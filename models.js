ab06d6d95550b326dffb8d3b67deea3a4ba526c4
kuolun <kuolun@gmail.com>
2016-03-03 00:25:26 +0800

    index.js to server.js and move

---
+++ models.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var _ = require('underscore');
+
+module.exports = function(wagner) {
+    // mongoose.connect('mongodb://localhost:27017/test');
+    mongoose.connect('mongodb://kuolun:100767@ds019068.mlab.com:19068/heroku_171pns0z');
+
+    wagner.factory('db', function() {
+        return mongoose;
+    });
+
+    var Category =
+        mongoose.model('Category', require('./category'), 'categories');
+    var User =
+        mongoose.model('User', require('./user'), 'users');
+
+    var models = {
+        Category: Category,
+        User: User
+    };
+
+    // To ensure DRY-ness, register factories in a loop
+    _.each(models, function(value, key) {
+        wagner.factory(key, function() {
+            return value;
+        });
+    });
+
+    wagner.factory('Product', require('./product'));
+
+    return models;
+};
